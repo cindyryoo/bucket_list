97142b61ae6c26b7c6efeca3c33efa628f7a605a
cindyryoo <cindyryoo1@gmail.com>
2021-01-03 14:54:20 +0100

    adjust deleteBtn JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const navbaritems = document.querySelectorAll('.navbar-item');
+const navbaritems = document.querySelectorAll('.navbar__item');
 const options = document.querySelectorAll('.type__option');
 const items = document.querySelector('.items');
 const typeToggle = document.querySelector('.type__toggle');
@@ -12,14 +12,13 @@ const addBtn = document.querySelector('.footer__button');
 
 function onAdd() {
     const text = input.value;
-    console.log(text);
     if (text ==='') {
         input.focus();
         return;
     }
     const item = createItem(text);
         items.appendChild(item);
-        item.scrollIntoView();
+        item.scrollIntoView({block: 'center'});
         input.value = '';
         input.focus();
 }
@@ -27,12 +26,14 @@ function onAdd() {
 let id = 0;
 function createItem(text) {
     const itemRow = document.createElement('li');
-        itemRow.setAttribute('class','item__row');
+        itemRow.setAttribute('class', 'item__row');
         itemRow.setAttribute('data-id',id);
         itemRow.innerHTML = ` 
             <div class="item">
-                <input type="checkbox" name="checkbox" id="check">
-                <span class="item__name">${text}</span>
+                <span class="item__name">
+                    <input type="checkbox" name="checkbox" id="check">
+                    ${text}
+                </span>
                 <button class="item__delete">
                     <i class="fas fa-trash-alt" data-id=${id}></i>
                 </button>
@@ -43,16 +44,16 @@ function createItem(text) {
             return itemRow;
 }
 
-addBtn.addEventListener('click',onAdd);
+addBtn.addEventListener('click', onAdd);
 
-input.addEventListener('keypress', event => {
+input.addEventListener('keyup', event => {
     if (event.key === 'Enter') {
         onAdd();
     }
 })
 
 items.addEventListener('click', event => {
-    const id = event.target.id;
+    const id = event.target.dataset.id;
     if(id) {
         const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
         toBeDeleted.remove();
